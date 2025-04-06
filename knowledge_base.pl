cedaf45f17fdcc63d0b3ede63b999b3111c71ab9
Alchimis <79978300+Alchimis@users.noreply.github.com>
2025-04-07 02:22:51 +0300

    add some models

---
+++ knowledge_base.pl
@@ -0,0 +1,18 @@
+/**
+ * large_model\2 Id, ModelName
+ */
+large_model(1, deepSeek).
+large_model(2, chatGpt).
+large_model(3, claude).
+large_model(4, bert).
+large_model(5, midjourney).
+large_model(7, mistral).
+large_model(8, dall-e).
+
+
+
+/**
+ * modality\2 Id, [Modality]
+ */
+modality(1, [texts, documents, tables]).
+modality(2, [texts, images, voice]).
