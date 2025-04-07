4b8e1082cd7917d16245efb95acd0efd7b0876d4
viktor.pestov <viktor.pestov@whitetigersoft.ru>
2025-04-07 14:55:28 +0300

    new data

--- knowledge_base.pl
+++ knowledge_base.pl
@@ -6,8 +6,27 @@ large_model(2, chatGpt).
 large_model(3, claude).
 large_model(4, bert).
 large_model(5, midjourney).
-large_model(7, mistral).
-large_model(8, dall-e).
+large_model(6, mistral).
+large_model(7, dall-e).
+large_model(8, bard).
+large_model(9, llama).
+
+
+/**
+ * version\2 VersionId, ModelId, VersionName
+ */
+version(1,  1, deepSeek_7B).
+version(2,  1, deepSeek_67B).
+version(3,  1, deepSeek_v2).
+version(4,  1, deepSeek_v3).
+version(5,  1, deepSeek_r1).
+version(6,  2, gpt_1).
+version(7,  2, gpt_2).
+version(8,  2, gpt_3).
+version(9,  2, gpt_4).
+version(10, 3, claude_1).
+version(11, 3, claude_2).
+version(12, 3, claude_3).
 
 
 
@@ -17,3 +36,47 @@ large_model(8, dall-e).
 modality(1, [texts, documents, tables]).
 modality(2, [texts, documents, images, voice]).
 modality(3, [texts, documents, images]).
+modality(4, [texts]).
+modality(5, [texts, images]).
+modality(6, [texts]).
+modality(7, [texts, images]).
+modality(8, [texts]).
+
+ 
+ /**
+  * model_version_size\2 VersionId, [Size]
+  */
+model_version_size(1, ["7B", "7 billions", "7 миллиардов"]).
+model_version_size(2, ["67B", "67 billions", "67 миллиардов"]).
+model_version_size(3, ["236B", "236 billions", "236 миллиардов"]).
+model_version_size(4, ["671B", "671 billions", "671 миллиардов"]).
+model_version_size(5, ["671B", "671 billions", "671 миллиардов"]).
+model_version_size(6, ["117M", "117 millions", "117 миллионов"]).
+model_version_size(7, ["1.5B", "1.5 billions", "1.5 миллиарда"]).
+model_version_size(8, ["175B", "175 billions", "175B миллиардов"]).
+model_version_size(9, ["1.76T", "1.76 trillions", "1.76 триллионов"]).
+
+/**
+ * type\2 TypeId, Type 
+ */
+type(1, generativeModel).
+
+/**
+ * role\2 ModelId, [Role]
+ */
+role(1, ["Генерирует "]).
+
+
+
+/**
+ * context_windows_size\2 VersionId, [Size]
+ */
+
+
+
+/**
+ * availability\2 Availability, [ModelId]
+ */
+availability(proprietary, [2, 3, 5, 7]).
+availability(openSource, [4, 6, 9]).
+availability(limitedAccess, [1, 8]).
